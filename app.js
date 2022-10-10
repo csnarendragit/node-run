ca56ab0a9fd94c3ecc96fc1107c9a96c5e32f574
csnarendragit <80242991+csnarendragit@users.noreply.github.com>
2022-10-10 09:24:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - this is git-jenkins connected');
+  res.send('Hello World - this is git-jenkins connected - it will');
 });
 
 app.listen(PORT, HOST);
